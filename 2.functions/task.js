c2add693a51821d288b372364f06064f1a957d8c
Ulitka62 <vadborisov62@gmail.com>
2022-07-19 21:07:10 +0300

    fixed the syntax

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,5 +1,6 @@
 /// Задание 1
-function getArrayParams(arr) {
+
+function getArrayParams(arr){
   let min = Infinity;
   let max = -Infinity;
   let sum = 0;
@@ -7,50 +8,48 @@ function getArrayParams(arr) {
   for (let i=0; i<arr.length; i++){
     if (max<arr[i]){
       max=arr[i];
-    }if (min>arr[i]){
+    } if (min>arr[i]){
       min=arr[i]
-    };
-    sum+=arr[i]; 
+    }
+    sum+=arr[i];
   }
   avg = sum/arr.length;
   avg = Number(avg.toFixed(2));
   return { min: min, max: max, avg: avg };
-};
-
-// Задание 2
-function worker(arr) {
-  let sum=0;
-for(let i=0;i<arr.length; i++){
-  sum+=arr[i];
 }
+// Задание 2
 
+function worker(arr){
+  let sum=0;
+  for(let i=0;i<arr.length; i++){
+    sum+=arr[i];
+  }
   return sum;
 }
 
-function makeWork(arrOfArr, worker) {
+function makeWork(arrOfArr, worker){
   let max = -Infinity;
- 
+
   for (let i=0; i<arrOfArr.length;i++){
-    if (max<worker(arrOfArr[i])) {
+    if (max<worker(arrOfArr[i])){
       max = worker(arrOfArr[i]);
     }
   }
   return max;
-};
-
+}
 // Задание 3
-function worker2(arr) {
+function worker2(arr){
   let min = max = arr[0];
 
   for(let i=0;i<arr.length; i++){
     if (max<arr[i]){
       max=arr[i];
+    }
+    if (min>arr[i]){
+      min=arr[i];
+    }
   }
-  if (min>arr[i]){
-  min=arr[i];
-}
-}
-return max-min;
+  return max-min;
 }
 
 //worker
