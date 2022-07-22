5499cac1b2bd951dd4fa129b4bdb956667fad599
Ulitka62 <vadborisov62@gmail.com>
2022-07-22 09:52:01 +0300

    solving homework

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,11 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
+  let result = JSON.stringify(arr1) === JSON.stringify(arr2);
   return result; // boolean
+  console.log(result);
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
+  let resultArr = arr.filter(item => item >= 0 && item % 3 === 0).map(item => item * 10);
   return resultArr; // array
+  console.log(resultArr);
 }
